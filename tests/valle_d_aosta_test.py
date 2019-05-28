9467dc67f6e49c1dc8009e30c01ce73fbf3eb3dd
Nicolas Lazzari <nicolaslazzari98@gmail.com>
2019-05-28 12:46:50 +0200

    changed test suite and implementations

---
+++ tests/valle_d_aosta_test.py
@@ -0,0 +1,26 @@
+import unittest
+import datetime
+from regions.valle_d_aosta import ValleDAosta
+
+class ValleDAostaTest(unittest.TestCase):
+    region_cls = ValleDAosta
+    air_quality = {
+        'Aosta': {'SO2': 12.0, 'NO2': 43.42, 'O3': 53.88, 'Pm10': 27.8, 'Pm2,5': 25.0, 'C6H6': 2.2 }
+    }
+
+    def test_air_quality(self):
+        date = datetime.datetime(year=2019, month=1, day=1)
+
+        region = self.region_cls()
+        region.fetch_air_quality(date)
+        region.wait_for_quality()
+
+        for province in region.provinces:
+            quality = province.quality.asdict()
+            expected = self.air_quality[province.name]
+
+            for key in expected.keys():
+                self.assertEqual(quality[key], expected[key])
+
+if __name__ == '__main__':
+    unittest.main()
